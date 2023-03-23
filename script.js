ee3af0c0259db0720d95456b0d62c9a491e183f4
Marcus Vinicius de Jesus Tourinho <viniciustourinho01@gmail.com>
2023-03-23 17:47:58 -0300

    Requisitos 3 e 4

---
+++ script.js
@@ -0,0 +1,16 @@
+window.onload = () => {
+  const btnSubmit = document.getElementById("password-button");
+
+  const getLogin = () => {
+    console.log('clicou');
+    const email = document.getElementById("email");
+    const password = document.getElementById("password");
+    if (email.value !== "tryber@teste.com" || password.value !== "123456") {
+      alert("Email ou senha inválidos.");
+    } else {
+      alert("Olá, Tryber!");
+    }
+  };
+
+  btnSubmit.addEventListener("click", getLogin);
+};
